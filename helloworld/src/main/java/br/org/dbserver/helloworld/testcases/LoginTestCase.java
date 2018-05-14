d9ac1a3cca8153f8278d817809ec43e91de10a48
Adriano <adrianochaves93@gmail.com>
2018-05-14 16:00:00 -0300

    adicionado login automatizado

--- helloworld/src/main/java/br/org/dbserver/helloworld/testcases/LoginTestCase.java
+++ helloworld/src/main/java/br/org/dbserver/helloworld/testcases/LoginTestCase.java
@@ -3,9 +3,10 @@ package br.org.dbserver.helloworld.testcases;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTestCase {
@@ -20,9 +21,16 @@ public class LoginTestCase {
 	
 	@Test
 	public void testMain() {
+		String usuario = "demo";
+		String senha = "demo";
+ 		WebElement usernameTextField = this.driver.findElement(By.id("modlgn-username"));
+		usernameTextField.sendKeys(usuario);
 		
+		WebElement passwordTextField = this.driver.findElement(By.id("modlgn-passwd"));
+		passwordTextField.sendKeys(senha);
 		
-		
+		WebElement buton = this.driver.findElement(By.name("Submit"));
+		buton.click();
 	}
 	
 	@After
